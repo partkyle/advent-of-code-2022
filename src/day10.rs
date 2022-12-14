91a0f52d4854f6eb0eb18c124a522ea1187c4b7b
Kyle Partridge <partkyle@gmail.com>
2022-12-13 20:32:14 -0800

    crt baby

--- src/day10.rs
+++ src/day10.rs
@@ -2,7 +2,7 @@
 pub mod day10 {
     use std::{collections::HashMap, str::FromStr};
 
-    use itertools::{zip_eq, Itertools};
+    use itertools::Itertools;
 
     #[derive(Debug)]
     enum Instruction {
@@ -38,16 +38,13 @@ pub mod day10 {
         }
     }
 
-    pub fn part1(text: String) -> Result<i32, Box<dyn std::error::Error>> {
-        let instructions: Result<Vec<Instruction>, _> =
-            text.lines().map(|line| line.parse()).collect();
-
+    fn run_instructions(instructions: Vec<Instruction>) -> HashMap<i32, i32> {
         let mut x = 1;
         let mut cycle = 1;
         let mut cycles = HashMap::new();
         cycles.insert(cycle, x);
 
-        for inst in instructions? {
+        for inst in instructions {
             cycle += inst.cycles();
 
             match inst {
@@ -59,20 +56,66 @@ pub mod day10 {
             };
         }
 
+        cycles
+    }
+
+    fn read_instructions(text: String) -> Result<Vec<Instruction>, Box<(dyn std::error::Error)>> {
+        text.lines().map(|line| line.parse()).collect()
+    }
+
+    fn get_cycle_value_at(cycles: &HashMap<i32, i32>, i: i32) -> i32 {
+        let key = cycles.keys().filter(|&&k| k <= i).sorted().last().unwrap();
+        *cycles.get(key).unwrap()
+    }
+
+    pub fn part1(text: String) -> Result<i32, Box<dyn std::error::Error>> {
+        let instructions = read_instructions(text)?;
+
+        let cycles = run_instructions(instructions);
+
         let important_values = [20, 60, 100, 140, 180, 220];
         let result = important_values
             .iter()
-            .map(|&i| {
-                let key = cycles.keys().filter(|&&k| k <= i).sorted().last().unwrap();
-                i * cycles.get(key).unwrap()
-            })
+            .map(|&i| i * get_cycle_value_at(&cycles, i))
             .sum();
 
         Ok(result)
     }
 
-    pub fn part2(text: String) -> Result<isize, Box<dyn std::error::Error>> {
-        todo!("not doing it");
+    fn cycle_to_scanline(i: i32) -> i32 {
+        (i - 1) % 40
+    }
+
+    pub fn part2(text: String) -> Result<String, Box<dyn std::error::Error>> {
+        let scan_lines: Vec<Vec<i32>> = (0..6)
+            .map(|i| {
+                let start = i * 40;
+                (start + 1..=start + 40).collect()
+            })
+            .collect();
+
+        let instructions = read_instructions(text)?;
+        let cycles = run_instructions(instructions);
+
+        let result: Vec<String> = scan_lines
+            .iter()
+            .map(|line| {
+                line.iter()
+                    .map(|&i| {
+                        let x = get_cycle_value_at(&cycles, i);
+
+                        let scanvalue = cycle_to_scanline(i);
+                        if x - 1 <= scanvalue && scanvalue <= x + 1 {
+                            '#'
+                        } else {
+                            '.'
+                        }
+                    })
+                    .collect()
+            })
+            .collect();
+
+        Ok(result.join("\n"))
     }
 }
 
