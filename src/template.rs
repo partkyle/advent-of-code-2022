f3ca68b5ba1a8ee450fc357344ec6e527413515a
partkyle <partkyle@gmail.com>
2022-12-14 23:20:06 -0800

    template update

--- src/template.rs
+++ src/template.rs
@@ -1,29 +1,29 @@
 #[cfg(test)]
 pub mod template {
-    pub fn part1(text: String) -> Result<isize, Box<dyn std::error::Error>> {
+    pub fn part1(_text: String) -> Result<isize, Box<dyn std::error::Error>> {
         todo!("need to do it");
     }
 
-    pub fn part2(text: String) -> Result<isize, Box<dyn std::error::Error>> {
+    pub fn part2(_text: String) -> Result<isize, Box<dyn std::error::Error>> {
         todo!("not doing it");
     }
-}
 
-#[cfg(test)]
-mod test {
-    use crate::helper::helper::run_day;
+    #[cfg(test)]
+    mod test {
+        use crate::helper::helper::run_day;
 
-    use super::template;
+        use super::*;
 
-    const DAY: usize = 1;
+        const DAY: usize = 1;
 
-    #[test]
-    fn part1() {
-        run_day(DAY, template::part1);
-    }
+        #[test]
+        fn test_part1() {
+            run_day(DAY, part1);
+        }
 
-    #[test]
-    fn part2() {
-        run_day(DAY, template::part2);
+        #[test]
+        fn test_part2() {
+            run_day(DAY, part2);
+        }
     }
 }
